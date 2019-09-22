cf5bb9aa3454ce00d35660882ef6a03f20b48b94
Sejal Bhatia <sejalbhatia30@gmail.com>
2019-09-22 13:14:09 +0530

    final

--- util/config.js
+++ util/config.js
@@ -3,14 +3,14 @@ cloudinary.config({
     // cloud_name: 'ssadh',
     // api_key: '187384484318298',
     // api_secret: '05aN_lmywO1P6AOeL7buOU-EKb0'
-    // api_key: '886581635645134',
+    api_key: '123877529735436',
 
-    // api_secret: '02FoejRxuwHyqzjkokLeouyK_KE',
+    api_secret: 'uEMOikt8VePttcfSDUca_SxrplI',
 
-    // cloud_name: 'sarthak-sadh'
-    cloud_name:'ankit-jain',
-    api_key:'975843617248193',
-    api_secret:'qacrsmsZRGQs8BCBRDLcvPZlibg'
+    cloud_name: 'sarthaksadh'
+    // cloud_name:'ankit-jain',
+    // api_key:'975843617248193',
+    // api_secret:'qacrsmsZRGQs8BCBRDLcvPZlibg'
 });
 
 module.exports = {
